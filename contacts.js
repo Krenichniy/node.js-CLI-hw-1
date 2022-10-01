a0230c92335a61b95899228a2457444c716f13d6
Yevhen <73534149+Krenichniy@users.noreply.github.com>
2022-10-01 12:57:54 +0300

    added last changes

--- contacts.js
+++ contacts.js
@@ -1,30 +1,53 @@
-const fs = require('fs').promises;
-const path = require('path').promises;
+const fs = require('fs/promises');
+const { nanoid } = require('nanoid');
 
-const contactsPath = './db/contacts.json';
+const path = require('path')
+const contactsPath = path.join(__dirname,'db','contacts.json');
 
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  // ...твой код
+async function updateContatcs (contacts) {
+   await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+}
+async function listContacts () {
+  const result = await fs.readFile(contactsPath)
+  return JSON.parse(result);
 }
 
-function getContactById(contactId) {
-  // ...твой код
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const contactById = contacts.find(el => el.id === contactId);
+  return contactById || null;
 }
 
-function removeContact(contactId) {
-  // ...твой код
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(contact => contact.id === contactId);
+  if (index === -1) {
+    return null
+  }
+  const [removedContact] = contacts.splice(index, 1)
+  await updateContatcs(contacts);
+  return removedContact;
+
 }
 
-function addContact(name, email, phone) {
-  // ...твой код
+async function addContact({name, email, phone}) {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name, 
+    email,
+    phone
+  }
+  contacts.push(newContact);
+  await updateContatcs(contacts)
+  return newContact ;
 }
 
-const contactsFn = {
+
+module.exports =  {
     listContacts, 
     getContactById,
     removeContact,
     addContact
-}
-
-module.exports = contactsFn;
+};
--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-const contacts = require('./contacts');
+const contatcs = require('./contacts');
+const { program } = require('commander');
+require('colors');
+program
+    .option('-a, --action <type>')
+    .option('-id, --id <type>')
+    .option('-n, --name <type>')
+    .option('-e, --email <type>')
+    .option('-ph, --phone <type>');
+program.parse();
+const options = program.opts();
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+          const allContacts = await contatcs.listContacts();
+          console.log(allContacts)
+      break;
+
+    case "getById":
+          const searchedContact = await contatcs.getContactById(id);
+          console.log('You found contact!:'.yellow,searchedContact )
+      break;
+      
+    case "add":
+          const newContact = await contatcs.addContact({ name, email, phone })
+          console.log('You added new contact'.green,newContact)
+      break;
+
+    case "remove":
+          const removedContact = await contatcs.removeContact(id);
+          console.log('You removed  contact('.green,removedContact)
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(options);
